8b904eddd2ea4417a08bbefb5cb03ee3c5620e80
Mala K <karmakarmala@gmail.com>
2024-11-25 09:27:42 +0100

    Refactor input file paths and update setup script for conda environment setup

--- rob-app/setup.sh
+++ rob-app/setup.sh
@@ -1,15 +1,13 @@
 set -e
-# echo "Setting conda environment.."
-# cd rob-pome/rob-app
-# conda env create --file env_rob.yaml
-# conda activate rob
-
+echo "Setting conda environment.."
+cd rob-pre/rob-app
+conda env create --file env_rob.yaml
+conda activate rob
 
 echo "Downloading spacy module.."
 python -m spacy download en_core_web_sm
 
 echo "====================================================="
 echo "Loading pre-trained weights.."
-curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=18YixZQ4otcZWdAMavy5OviR0579kWrCm" > /tmp/intermezzo.html
-curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth/dsc_w0.pth.tar
+gdown "https://drive.google.com/uc?id=18YixZQ4otcZWdAMavy5OviR0579kWrCm" -O pth/dsc_w0.pth.tar
 echo "Finished."
